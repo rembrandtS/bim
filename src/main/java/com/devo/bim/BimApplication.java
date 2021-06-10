a023f2e1ae5b1bedb2ae7173b4c8b1d0452a095d
rembrandtS <remblandt@gmail.com>
2021-06-10 13:51:15 +0900

    Initial commit

---
+++ src/main/java/com/devo/bim/BimApplication.java
@@ -0,0 +1,13 @@
+package com.devo.bim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eucast/bim/BimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eucast.bim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
