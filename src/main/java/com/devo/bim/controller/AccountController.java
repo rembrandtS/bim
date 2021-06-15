ee94dc5844d73231aa0e5a4bca825c0d8598013d
rembrandtS <remblandt@gmail.com>
2021-06-15 18:52:24 +0900

    feat : 초기 셋팅 (fragment로 작업중...)

--- src/main/java/com/devo/bim/controller/AccountController.java
+++ src/main/java/com/devo/bim/controller/AccountController.java
@@ -5,10 +5,8 @@ import com.devo.bim.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/account")
@@ -36,5 +34,4 @@ public class AccountController {
 
         return "account/login";
     }
-
 }
