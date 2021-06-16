7f3523e742149eb15bbb6b6e7c465ac41480b3a8
rembrandtS <remblandt@gmail.com>
2021-06-16 15:21:58 +0900

    feat : 로그인 언어 선택 기능 추가

--- src/main/java/com/devo/bim/controller/AccountController.java
+++ src/main/java/com/devo/bim/controller/AccountController.java
@@ -3,10 +3,13 @@ package com.devo.bim.controller;
 import com.devo.bim.model.Account;
 import com.devo.bim.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/account")
@@ -19,7 +22,9 @@ public class AccountController {
     private PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+
+        model.addAttribute("locale", LocaleContextHolder.getLocale());
         return "account/login";
     }
 
